bedfca271f9c62b6337043f15d7c3cc8777e655c
Lennon Garcia D. Bravo <lennon@sistemasbr.net>
2022-06-23 01:46:12 -0300

    feat: rock moves smothly in the scene.

--- main.lua
+++ main.lua
@@ -1,17 +1,41 @@
 function love.load()
+    anim8 = require 'libraries/anim8'
     love.graphics.setBackgroundColor(love.math.colorFromBytes(148, 217, 235))
+    love.graphics.setDefaultFilter("nearest", "nearest")
     love.keyboard.setKeyRepeat(true)
     rock = {}
     rock.x = 300
     rock.y = 644
-    rock.speed = 5
+    rock.speed = 0.4
     rock.spriteSheet = love.graphics.newImage("assets/sprites/objects/rock.png")
+    rock.grid = anim8.newGrid(52,52, rock.spriteSheet:getWidth(), rock.spriteSheet:getHeight())
+    rock.isMoving = false
+    rock.animations = {}
+    rock.animations.right = anim8.newAnimation(rock.grid("1-6", 1), 0.1)
+    rock.animations.left = anim8.newAnimation(rock.grid("6-1", 1), 0.1)
+    rock.currentAnimation = rock.animations.right
     timer = 0
-    keyboard_actions = {["right"] = move_rock_to_right, ["left"] = move_rock_to_left}
+    keyboard_actions_pressed = {["right"] = move_rock_to_right, ["left"] = move_rock_to_left}
+    keyboard_actions_released = {["right"] = stop_rock, ["left"] = stop_rock}
 end
 
 function love.update(dt)
+    local isMoving = false
     timer = timer + dt
+
+    if love.keyboard.isDown("right") then
+        move_rock_to_right(isMoving)
+    end
+
+    if love.keyboard.isDown("left") then
+        move_rock_to_left(isMoving)
+    end
+
+    if isMoving == false and rock.isMoving == false then
+        rock.currentAnimation:gotoFrame(1)
+    end
+
+    rock.currentAnimation:update(dt)
 end
 
 function love.draw()
@@ -19,26 +43,58 @@ function love.draw()
     love.graphics.rectangle("fill", 0, 670, 1280, 50)
 
     love.graphics.setColor(love.math.colorFromBytes(224, 13, 9))
-    love.graphics.print(timer, 40, 10, 0, 1, 1)
+    love.graphics.print(timer, 40, 10, nil, 3)
 
     love.graphics.setColor(1, 1, 1, 1)
-    --love.graphics.draw(rockImage, rockQuad, rock.x, rock.y)
+    rock.currentAnimation:draw(rock.spriteSheet, rock.x, rock.y)
 end
 
-function love.keypressed(key, scancode, isrepeat)
-    move_rock(key)
-end
-
-function move_rock(key)
-    if keyboard_actions[key] ~= nil then
-        keyboard_actions[key]()
-    end
+function move_rock_to_right(isMoving)
+    move_rock_to_right()
+    isMoving = true
 end
 
 function move_rock_to_right()
     rock.x = rock.x + rock.speed
+    rock.currentAnimation = rock.animations.right
+    rock.isMoving = true
+end
+
+function move_rock_to_left(isMoving)
+    move_rock_to_left()
+    isMoving = true
 end
 
 function move_rock_to_left()
     rock.x = rock.x - rock.speed
+    rock.currentAnimation = rock.animations.left
+    rock.isMoving = true
+end
+
+function love.keypressed(key, scancode, isrepeat)
+    move_rock(key)
+    if isrepeat == false then
+        rock.isMoving = false
+    end
+end
+
+function move_rock(key)
+    if keyboard_actions_pressed[key] ~= nil then
+        keyboard_actions_pressed[key]()
+    end
+end
+
+function love.keyreleased(key)
+    stop_rock(key)
+end
+
+function stop_rock(key)
+    if keyboard_actions_released[key] ~= nil then
+        keyboard_actions_released[key]()
+    end
+end
+
+function stop_rock()
+    rock.currentAnimation:gotoFrame(1)
+    rock.isMoving = false
 end
