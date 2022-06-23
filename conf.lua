250ba80030df006a7b3bca2f25f2adfe0a92dad3
Lennon Garcia D. Bravo <lennon@sistemasbr.net>
2022-06-22 21:23:51 -0300

    feat: creating conf.lua file

---
+++ conf.lua
@@ -0,0 +1,13 @@
+function love.conf(t)
+    t.console = false
+    t.version = "0.0.1"
+    t.window.title = "AbracaBrawl"
+    --t.window.icon = nil
+    t.window.width = 600
+    t.window.height = 600
+    t.window.borderless = false
+    t.window.resizable = false
+    t.window.fullscreen = false
+    t.window.vsync = 0
+    t.window.msaa = 8
+end
